40d22a57eb80095230b850721808de22963f8e47
Vasyl-KR <w.kyrychuk@gmail.com>
2018-10-10 22:47:28 +0300

    PageObject added

--- TestProject/tests/google.js
+++ TestProject/tests/google.js
@@ -1,29 +1,20 @@
 module.exports = {
 
-    tags: ['google'],
- 
-    'Demo test Google' : function (browser) {
- 
-         browser 
- 
-         .url('http://www.google.com') // Go to a url
- 
-         .waitForElementVisible('body', 1000) // wait till page loads
- 
-         .assert.title('Google') // Make sure Site title matches
- 
-         .assert.visible('input[type=text]') 
- 
-         .setValue('input[type=text]', 'nightwatchjs') // send values
- 
-         .click('button[name=btnG]') // click on search box
- 
-         .pause(1000)
- 
-         .end();
- 
-       }
- 
- };
- 
- // = comment not necessary for code
+  tags: ['google'],
+
+  'Demo test Google' : function (browser) {
+
+      var google = browser.page.googlePage();
+       
+
+      google.navigate()
+      .assert.title('Google')
+      .assert.visible('@searchBar')
+      .setValue('@searchBar', 'RetailMeNot')
+      .click('@submit');
+
+       browser.pause(2000).end();
+
+     }
+
+};
